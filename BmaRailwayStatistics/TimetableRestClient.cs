2e11a255ee1ea3901a2aceeca86db6049260d4b9
Pongrácz Lajos <l.pongracz@accenture.com>
2019-09-11 13:45:42 +0200

    removed

--- BmaRailwayStatistics/TimetableRestClient.cs
+++ BmaRailwayStatistics/TimetableRestClient.cs
@@ -16,7 +16,7 @@ namespace BmaRailwayStatistics
         public async Task<string> GetTimetableBetweenEndpoints(string start, string end)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            var url = $"http://elvira.mav-start.hu/elvira.dll/uf?i={start}&e={end}&sb=1&sk=5&mikor=0";
+            var url = $"";
             var response = await client.GetAsync(url);
             return Encoding.GetEncoding("windows-1250").GetString(await response.Content.ReadAsByteArrayAsync());
         }
